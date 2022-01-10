82c13e32b513e91177a2242e755d7a3a885dadce
yezii <1126yezy@gmail.com>
2022-01-10 21:02:49 +0900

    Tailwind CSS Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-red-500 text-center">
+      <h1 className="text-white">hello tailwind css</h1>
+    </div>
+  );
 };
 
 export default Home;
