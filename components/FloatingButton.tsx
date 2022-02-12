13e8107695a28d718ab60c89c84348d30a83ded7
yejiiha <1126yezy@gmail.com>
2022-02-12 20:12:01 +0900

    Fix UI: Floating Button

--- components/FloatingButton.tsx
+++ components/FloatingButton.tsx
@@ -11,10 +11,12 @@ export default function FloatingButton({
   href,
 }: FloatingButtonProps) {
   return (
-    <Link href={href}>
-      <a className="fixed bottom-24 right-5 bg-purple-400 rounded-full text-white p-4 shadow-xl hover:bg-purple-300 transition-colors border-transparent cursor-pointer w-14 flex items-center justify-center">
-        {children}
-      </a>
-    </Link>
+    <div className="flex justify-end pr-5">
+      <Link href={href}>
+        <a className="fixed bottom-24 bg-purple-400 rounded-full text-white p-4 shadow-xl hover:bg-purple-300 transition-colors border-transparent cursor-pointer w-14 flex items-center justify-center">
+          {children}
+        </a>
+      </Link>
+    </div>
   );
 }
